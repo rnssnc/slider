293e20e726db495d64ba7bb25112a0b00360fede
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-03-08 13:33:03 +0300

    feat: drag n drop draft version

--- script.js
+++ script.js
@@ -23,16 +23,9 @@ class Slider {
       this.addClones(this.slidesToShow);
       this.transformValue = this.defaultTranslate;
       // Listeners
-      this.buttonNext = document.querySelector('.control.next').addEventListener('click', (e) => {
-        if (this.track.style.transition != null)
-          if (this.index < this.slides.length - this.slidesToShow)
-            this.shiftSlide(this.slidesToScroll) || e.preventDefault();
-      });
+      this.buttonNext = document.querySelector('.control.next').addEventListener('click', this.nextSlide);
 
-      this.buttonPrev = document.querySelector('.control.prev').addEventListener('click', (e) => {
-        if (this.track.style.transition != null)
-          if (this.index >= this.slidesToShow) this.shiftSlide(-this.slidesToScroll) || e.preventDefault();
-      });
+      this.buttonPrev = document.querySelector('.control.prev').addEventListener('click', this.prevSlide);
     } else {
       this.buttonNext = document.querySelector('.control.next').addEventListener('click', (e) => {
         if (this.index < this.slides.length) this.shiftSlide(1) || e.preventDefault();
@@ -45,24 +38,81 @@ class Slider {
 
     this.setTrackWidth(this.track);
 
-    this.track.addEventListener('transitionend', (e) => {
-      if (e.propertyName == 'transform') {
-        this.track.style.transition = null;
-        if (this.infinite && this.index == this.slides.length - this.slidesToShow) {
-          this.transformValue = this.defaultTranslate;
-          this.track.style.transform = `translateX(${this.defaultTranslate}px)`;
-          this.index = this.slidesToShow;
-        } else if (this.infinite && this.index == 0) {
-          this.transformValue = -this.widthWithoutClones;
-          this.track.style.transform = `translateX(${-this.widthWithoutClones}px)`;
-          this.index = this.defaultLength;
-        }
-      }
-    });
-
     // createTrack();
     this.fitSlides(this.slides);
+
+    this.track.addEventListener('transitionend', this.handleTransitionEnd);
+
+    this.shiftX = 0;
+    this.newLeft = 0;
+    this.posX1 = 0;
+    this.posX2 = 0;
+    this.track.addEventListener('pointerdown', (e) => {
+      e.preventDefault(); // prevent selection start (browser action)
+
+      this.shiftX = e.clientX - this.track.getBoundingClientRect().left;
+      this.posX1 = e.clientX;
+      this.track.setPointerCapture(e.pointerId);
+
+      this.track.addEventListener('pointermove', this.handlePointerMove);
+
+      this.track.addEventListener('pointerup', this.handlePointerUp);
+    });
   }
+
+  nextSlide = (e) => {
+    if (this.index < this.slides.length - this.slidesToShow)
+      this.shiftSlide(this.slidesToScroll) || e.preventDefault();
+  };
+
+  prevSlide = (e) => {
+    console.log('always');
+    if (this.index >= this.slidesToShow) this.shiftSlide(-this.slidesToScroll) || e.preventDefault();
+  };
+  handlePointerUp = (e) => {
+    this.track.releasePointerCapture(e.pointerId);
+    this.track.removeEventListener('pointermove', this.handlePointerMove);
+    if (this.track.style.transition != null)
+      if (this.transformValue + this.newLeft < this.transformValue) this.nextSlide(e);
+      else this.prevSlide(e);
+    this.track.removeEventListener('pointerup', this.handlePointerUp);
+  };
+
+  handlePointerMove = (e) => {
+    this.newLeft = e.clientX - this.shiftX - this.track.getBoundingClientRect().left;
+    console.log(this.newLeft);
+    // this.posX2 = this.posX1 - e.clientX;
+    // this.posX1 = e.clientX;
+    // console.log(this.newLeft);
+    // console.log(this.transformValue);
+
+    // если указатель находится за пределами слайдера => отрегулировать "left", чтобы оставаться в пределах границ
+    // if (this.newLeft < 0) {
+    //   this.newLeft = 0;
+    // }
+    // let rightEdge = this.slider.offsetWidth - this.track.offsetWidth;
+    // if (this.newLeft > this.rightEdge) {
+    //   this.newLeft = this.rightEdge;
+    // }
+    console.log(this.transformValue + this.newLeft);
+    this.track.style.transform = `translateX(${this.transformValue + this.newLeft}px)`;
+  };
+
+  handleTransitionEnd = (e) => {
+    if (e.propertyName == 'transform') {
+      this.track.style.transition = null;
+      if (this.infinite && this.index == this.slides.length - this.slidesToShow) {
+        console.log('rightyo');
+        this.transformValue = this.defaultTranslate;
+        this.track.style.transform = `translateX(${this.defaultTranslate}px)`;
+        this.index = this.slidesToShow;
+      } else if (this.infinite && this.index == 0) {
+        this.transformValue = -this.widthWithoutClones;
+        this.track.style.transform = `translateX(${-this.widthWithoutClones}px)`;
+        this.index = this.defaultLength;
+      }
+    }
+  };
 
   addClones(slidesToShow) {
     let appendNode = [];
@@ -79,7 +129,7 @@ class Slider {
       this.defaultTranslate -= this.slideWidth;
     }
     this.track.append(...appendNode);
-    this.track.prepend(...prependNode);
+    this.track.prepend(...prependNode.reverse());
     this.track.style.transform = `translateX(${this.defaultTranslate}px)`;
   }
 
@@ -94,6 +144,7 @@ class Slider {
   }
 
   shiftSlide(count) {
+    console.log(count);
     this.transformValue += -count * this.slideWidth;
     this.track.style.transition = `transform ${this.transitionTime}s`;
     this.track.style.transform = `translateX(${this.transformValue}px)`;
